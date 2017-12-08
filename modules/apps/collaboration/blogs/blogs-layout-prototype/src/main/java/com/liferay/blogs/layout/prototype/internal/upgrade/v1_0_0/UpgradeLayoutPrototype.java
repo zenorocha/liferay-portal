40a0a8591e5507ab20abc970f6c1bd4fd34a0184
leon.chi <leon.chi@liferay.com>
2017-12-08 11:10:53 +0800

    LPS-72169 Upgrade blogs-layout-prototype

---
+++ modules/apps/collaboration/blogs/blogs-layout-prototype/src/main/java/com/liferay/blogs/layout/prototype/internal/upgrade/v1_0_0/UpgradeLayoutPrototype.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.layout.prototype.internal.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradeLayoutPrototype;
+import com.liferay.portal.language.LanguageResources;
+
+/**
+ * @author Leon Chi
+ */
+public class UpgradeLayoutPrototype extends BaseUpgradeLayoutPrototype {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		upgradePrototype(
+			LanguageResources.RESOURCE_BUNDLE_LOADER, "LayoutPrototype", _NAME,
+			_DESCRIPTION, "layout-prototype-blog-title",
+			"layout-prototype-blog-description");
+	}
+
+	private static final String _DESCRIPTION =
+		"Create, edit, and view blogs from this page. Explore topics using " +
+			"tags, and connect with other members that blog.";
+
+	private static final String _NAME =
+		"<?xml version='1.0' encoding='UTF-8'?><root available-locales=" +
+			"\"en_US\" default-locale=\"en_US\"><Name language-id=\"en_US\">" +
+				"Blog</Name></root>";
+
+}
